86118473466ff6f4d35d0462a61cdd4ac6eacbb4
zli12321 <60415163+zli12321@users.noreply.github.com>
2023-08-23 10:48:06 -0400

    Add files via upload

---
+++ flask_app/static/track_labels.js
@@ -0,0 +1,73 @@
+// Start the timer for saving label data
+var timer = setInterval(saveLabels, 60000); // 1 minute (1 * 60 * 1000 milliseconds)
+
+var url = 'https://nist-topic-model.umiacs.umd.edu/save_user_label'
+// Function to save labels data
+let labelSeconds = localStorage.getItem("labelSeconds");
+if (!labelSeconds) {
+    labelSeconds = 0;
+}
+function saveLabels() {
+    labelSeconds += 60
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", url, false);
+    xhr.send(labelSeconds);
+    localStorage.setItem("labelSeconds", labelSeconds)
+}
+
+// Timer on navigation
+let seconds = localStorage.getItem("sessionSeconds");
+if (!seconds) {
+    seconds = 0;
+}
+
+function showReminderPopup30() {
+    alert("30 minutes have passed through the study!");
+}
+
+function showReminderPopup45() {
+    alert("45 minutes have passed through the study! You can start wrapping up at 55 minutes and take the survey");
+}
+
+let sessionTimer = setInterval(countUpSession, 1000);
+
+function countUpSession() {
+    ++seconds;
+
+    if (seconds === 1800) {
+        showReminderPopup30();
+    }
+    if (seconds === 2700) {
+        showReminderPopup30();
+    }
+
+
+    let hour = Math.floor(seconds / 3600);
+    let minute = Math.floor((seconds - hour * 3600) / 60);
+    let updSecond = seconds - (hour * 3600 + minute * 60);
+
+    if (updSecond < 10){
+        updSecond = "0" + updSecond;
+    }
+
+    if (minute < 10){
+        minute = "0" + minute;
+    }
+
+    if (hour < 10){
+        hour = "0" + hour;
+    }
+    
+        
+    document.getElementById("sessionTimer").innerHTML = hour + ":" + minute + ":" + updSecond;
+    localStorage.setItem("sessionSeconds", seconds)
+}
+
+let logoutButton = document.getElementById("finish");
+logoutButton.addEventListener("click", clearLocalStorage);
+
+function clearLocalStorage() {
+    seconds = 0;
+    localStorage.clear();
+}
+
