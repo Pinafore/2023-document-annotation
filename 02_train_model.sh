89ddf571e48559301531e969a620796d3e63e662
Zongxia Li <60415163+zli12321@users.noreply.github.com>
2023-11-07 09:58:51 -0500

    Added bash script to run the app

---
+++ 02_train_model.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+python ./flask_app/Topic_Models/train_models.py \
+        --num_topics 35 \
+        --num_iters 3000 \
+        --model_type LDA \
+        --save_trained_model_path ./flask_app/Topic_Models/trained_models/LDA_35.pkl
+
+python ./flask_app/Topic_Models/train_models.py \
+        --num_topics 35 \
+        --num_iters 3000 \
+        --model_type SLDA \
+        --save_trained_model_path ./flask_app/Topic_Models/trained_models/SLDA_35.pkl
+
+python ./flask_app/Topic_Models/train_models.py \
+        --num_topics 35 \
+        --num_iters 250 \
+        --model_type CTM \
+        --save_trained_model_path ./flask_app/Topic_Models/trained_models/CTM_35.pkl
